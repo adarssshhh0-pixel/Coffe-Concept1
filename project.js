cc432496273fe1363382447beb1d6be7e2091f86
Adarsh Hazari <adarssshhh0@gmail.com>
2025-12-10 04:52:43 +0530

    All js code is now written in it

--- project.js
+++ project.js
@@ -22,3 +22,45 @@ tll.from("#drop",{
     y:-20,
     duration:1.5,
 })
+
+function info()
+                {
+                    var x = document.getElementById("Emaill").value;
+                    var y = document.getElementById("Passwordd").value;
+                    
+                    if (x==""||y=="")
+                    {
+                        alert("All fields are required");
+                        return false;
+                    }
+                    else if( y.length<6 || y.length>6){
+                        alert("Passwordd must contains 6 number" +y.length);
+                        return false;
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                }
+
+function data()
+                {
+                    var a = document.getElementById("Email").value;
+                    var b = document.getElementById("Password").value;
+                    var c = document.getElementById("Confirm password").value;
+
+                if (a==""||b==""||c=="")
+                    {
+                        alert("All fields are required");
+                        return false;
+                    }
+                    else if( b !=c )
+                    {
+                        alert("Password do not match");
+                        return false;
+                    }
+                else
+                    {
+                        return true;
+                    }
+                }
